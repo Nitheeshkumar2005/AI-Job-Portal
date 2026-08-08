fcbfe8eb4b4af7196bfb90606516962f42cac01c
Nitheeshkumar2005 <balanitheesh2005@gmail.com>
2026-08-08 13:56:32 +0530

    case changes all

--- frontend/vite-project/src/pages/jobs.jsx
+++ frontend/vite-project/src/pages/jobs.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../utils/axiosInstance";
-import jobcards from "../components/jobCards";
+import jobcards from "../components/jobcards";
 import navbar from "../components/navbar";
 import searchbar from "../components/searchbar";
 
